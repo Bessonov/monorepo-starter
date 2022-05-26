f03612e6ccc954740808f5221c981c47538b3c1e
Anton Bessonov <anton.bessonov.public@gmail.com>
2022-05-26 20:17:35 +0200

    update dependencies

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-// must located here, see
-// https://github.com/webpack-contrib/postcss-loader/issues/405#issuecomment-694840006
-module.exports = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {},
-	}
-}
