6bbd287f46cc2f404b53331506c2f43c33e9fd6b
Anton Bessonov <anton.bessonov.public@gmail.com>
2023-11-17 16:03:47 +0000

    update dependencies
    - downgrade kill-port because of https://github.com/tiaanduplessis/kill-port/issues/61

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,24 +1,24 @@
 import http from 'http'
 import { backend } from 'shared'
-import micro, { sendError } from 'micro'
+import { send, serve } from 'micro'
 import {
-	Router,
 	EndpointMatcher,
+	NodeHttpRouter,
 } from '@bessonovs/node-http-router'
 
-const router = new Router((req, res) => sendError(req, res, { statusCode: 404 }))
+const router = new NodeHttpRouter(({ data: { res } }) => send(res, 404))
 
 const [address, port] = ['0.0.0.0', 3000]
 
-const server = http.createServer(micro(router.serve)).listen(port, address)
+const server = http.createServer(serve(router.serve)).listen(port, address)
 server.once('listening', () => {
 	// eslint-disable-next-line no-console
 	console.log(`started at http://${address}:${port}`)
 })
 
 router.addRoute({
-	matcher: new EndpointMatcher<{name: string}>('GET', /^\/hello\/(?<name>[^/]+)$/),
-	handler: (req, res, match) => {
-		return `Hello ${match.match.groups.name} ${backend()}!`
+	matcher: new EndpointMatcher<{ name: string }>('GET', /^\/hello\/(?<name>[^/]+)$/),
+	handler({ match: { result } }) {
+		return `Hello ${result.match.groups.name} ${backend()}!`
 	},
 })
